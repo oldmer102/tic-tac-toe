ba5c99b067d3d4eaf974d749766efc2efb047d7c
oldmer102 <56794465+oldmer102@users.noreply.github.com>
2022-07-19 13:01:42 +0300

    game_tic_tac_new3

--- main.py
+++ main.py
@@ -20,15 +20,16 @@ if int(bot_or_player) == 1:
         while not check.check_mistake(num):
             num = input('Введите номер поля: ')
         move = check.set_vell(num)
+        if check.check_win_x(move):
+            pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+            print(pole + '\nпобеда X')
+            break
+        if check.check_win_o(move):
+            pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+            print(pole + '\nпобеда O')
+            break
+
         if check.has_free(move, prever):
-            if check.check_win_x(move):
-                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
-                print(pole + '\nпобеда X')
-                break
-            if check.check_win_o(move):
-                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
-                print(pole + '\nпобеда O')
-                break
             pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
             print(pole)
             print('Ничья!')
@@ -47,15 +48,16 @@ if int(bot_or_player) == 2:
         while not check.check_mistake(num):
             num = input('Введите номер поля: ')
         move = check.set_vell(num)
+        if check.check_win_x(move):
+            pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+            print(pole + '\nпобеда X')
+            break
+        if check.check_win_o(move):
+            pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+            print(pole + '\nпобеда O')
+            break
+
         if check.has_free(move, prever):
-            if check.check_win_x(move):
-                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
-                print(pole + '\nпобеда X')
-                break
-            if check.check_win_o(move):
-                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
-                print(pole + '\nпобеда O')
-                break
             pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
             print(pole)
             print('Ничья!')
