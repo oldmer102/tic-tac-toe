6b811389da99ded4e353ebd3075b17bb8c10b984
oldmer102 <56794465+oldmer102@users.noreply.github.com>
2022-07-19 12:30:04 +0300

    game_tic_tac_new2

--- check.py
+++ check.py
@@ -1,41 +1,37 @@
 START = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 zapolnenie = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
-#zapolnenie = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', '-']
+#zapolnenie = ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', '-']
 WIN_KOMBO = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 
 
 
-def has_free(move, prevers):
-    if not check_win(move):
-        for i in range(0, 9):
-            if move[i] == 'X':
-                prevers += 1
-            if move[i] == 'O':
-                prevers += 1
-        if prevers == 9:
-            return True
-        else:
-            return False
+def has_free(move, prever):
+    for i in range(0, 9):
+        if move[i] == 'X':
+            prever += 1
+        if move[i] == 'O':
+            prever += 1
+    if prever == 9:
+        return True
     else:
         return False
 
-
-
-
-
+def check_plaers(num):
+    if num is not None and num.isdigit() and int(num) in range(1, 3):
+        print('Количество игроков:', num)
+        return True
 
 def check_mistake(num:str):
-
     if num is not None and num.isdigit() and int(num) in range(1,10):
         if zapolnenie[int(num) - 1] == '-':
             return True
     return False
 
 
-def set_vell(num):
-    zapolnenie[int(num)-1] = 'X'
+def set_vell(num, posit='X'):
+    zapolnenie[int(num)-1] = posit
     return zapolnenie
 
 
@@ -48,7 +44,8 @@ def check_win_x(move):
                 if prevecs == 3:
                     print('Победа X')
                     return True
-        return False
+    return False
+
 
 def check_win_o(move):
     for i in WIN_KOMBO:
@@ -59,7 +56,7 @@ def check_win_o(move):
                 if prevecs == 3:
                     print('Победа O')
                     return True
-        return False
+    return False
 
 
 
--- main.py
+++ main.py
@@ -2,18 +2,73 @@ import check
 import bot
 
 
+
 prefect = False
-pole = f"""{check.START[0]} | {check.START[1]} | {check.START[2]}\n{check.START[3]} | {check.START[4]} | {check.START[5]}\n{check.START[6]} | {check.START[7]} | {check.START[8]}\n"""
-print('Для хода введите номер поля, куда вы хотите поставить Х\n' + pole)
+prever = 0
+bot_or_player = None
 
-while not prefect:
-    num = None
-    while not check.check_mistake(num):
-        num = input('Введите номер поля: ')
-    move = check.set_vell(num)
-    move = bot.bot_tic_tac(move)
-    pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
-    print(pole)
-    prefect = check.check_win_x(move) or check.check_win_o(move)
 
+while not check.check_plaers(bot_or_player):
+    bot_or_player = input('1 или 2 игрока? ')
+
+#Один игрок
+if int(bot_or_player) == 1:
+    pole = f"""{check.START[0]} | {check.START[1]} | {check.START[2]}\n{check.START[3]} | {check.START[4]} | {check.START[5]}\n{check.START[6]} | {check.START[7]} | {check.START[8]}\n"""
+    print('Для хода введите номер поля, куда вы хотите поставить Х\n' + pole)
+    while not prefect:
+        num = None
+        while not check.check_mistake(num):
+            num = input('Введите номер поля: ')
+        move = check.set_vell(num)
+        if check.has_free(move, prever):
+            if check.check_win_x(move):
+                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+                print(pole + '\nпобеда X')
+                break
+            if check.check_win_o(move):
+                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+                print(pole + '\nпобеда O')
+                break
+            pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+            print(pole)
+            print('Ничья!')
+            break
+        move = bot.bot_tic_tac(move)
+        pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+        print(pole)
+        prefect = check.check_win_x(move) or check.check_win_o(move)
+
+#Два игрока
+if int(bot_or_player) == 2:
+    pole = f"""{check.START[0]} | {check.START[1]} | {check.START[2]}\n{check.START[3]} | {check.START[4]} | {check.START[5]}\n{check.START[6]} | {check.START[7]} | {check.START[8]}\n"""
+    print('Для хода введите номер поля, куда вы хотите поставить Х, первым ходит X потом O\n' + pole)
+    while not prefect:
+        num = None
+        while not check.check_mistake(num):
+            num = input('Введите номер поля: ')
+        move = check.set_vell(num)
+        if check.has_free(move, prever):
+            if check.check_win_x(move):
+                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+                print(pole + '\nпобеда X')
+                break
+            if check.check_win_o(move):
+                pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+                print(pole + '\nпобеда O')
+                break
+            pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+            print(pole)
+            print('Ничья!')
+            break
+
+        pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+        print(pole)
+        prefect = check.check_win_x(move) or check.check_win_o(move)
+        num = None
+        while not check.check_mistake(num):
+            num = input('Введите номер поля: ')
+        move = check.set_vell(num, posit='O')
+        pole = f"""{move[0]} | {move[1]} | {move[2]}\n{move[3]} | {move[4]} | {move[5]}\n{move[6]} | {move[7]} | {move[8]}\n"""
+        print(pole)
+        prefect = check.check_win_x(move) or check.check_win_o(move)
 
